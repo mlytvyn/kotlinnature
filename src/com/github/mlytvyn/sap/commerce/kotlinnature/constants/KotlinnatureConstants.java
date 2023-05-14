7b39e4f39ef5399b5a6157584ec9d504ffbd9717
Mykhailo Lytvyn <>
2023-05-14 16:24:01 +0200

    initial commit

---
+++ src/com/github/mlytvyn/sap/commerce/kotlinnature/constants/KotlinnatureConstants.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package com.github.mlytvyn.sap.commerce.kotlinnature.constants;
+
+/**
+ * Global class for all Kotlinnature constants. You can add global constants for your extension into this class.
+ */
+public final class KotlinnatureConstants extends GeneratedKotlinnatureConstants {
+    public static final String EXTENSIONNAME = "kotlinnature";
+
+    private KotlinnatureConstants() {
+        //empty to avoid instantiating this constant class
+    }
+
+}
